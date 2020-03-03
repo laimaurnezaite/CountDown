ed23273533e600dca70e533283d6d0e055a24e0d
Laima Urnėžaitė <laima@Laimas-MacBook-Pro.local>
2020-03-03 13:03:50 +0200

    add changes to correct date implementation

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import sqlite3
 from sqlite3 import Error
 from flask.templating import render_template
 from werkzeug.utils import redirect
+from datetime import date
+
 
 app = Flask(__name__)
 
@@ -12,7 +14,7 @@ def create_database(db_file):
     try:
         conn = sqlite3.connect(db_file)
         db = conn.cursor()
-        db.execute("CREATE TABLE IF NOT EXISTS events (id INTEGER NOT NULL, title TEXT NOT NULL, message TEXT, date TEXT NOT NULL, location TEXT, theme TEXT NOT NULL); ")
+        db.execute("CREATE TABLE IF NOT EXISTS events2 (id INTEGER NOT NULL, title TEXT NOT NULL, message TEXT, year TEXT NOT NULL, month TEXT NOT NULL, day TEXT NOT NULL, location TEXT, theme TEXT NOT NULL); ")
         # print(sqlite3.version)
     except Error as e:
         print(e)
@@ -44,10 +46,25 @@ def add_event():
     id = request.form.get("id")
     title = request.form.get("title")
     message = request.form.get("message")
-    date = request.form.get("date")
+    year = request.form.get("year")
+    month = request.form.get("month")
+    day = request.form.get("day")
     location = request.form.get("location")
     theme = request.form.get("theme")
     db = get_db()
-    db.execute("INSERT INTO events (id, title, message, date, location, theme) VALUES (:id, :title, :message, :date, :location, :theme);", {"id":id, "title":title, "message":message, "date":date, "location":location, "theme":theme})
+    db.execute("INSERT INTO events2 (id, title, message, year, month, day, location, theme) VALUES (:id, :title, :message, :year, :month, :day, :location, :theme);", {"id":id, "title":title, "message":message, "year":year, "month":month, "day":day, "location":location, "theme":theme})
     db.commit()
     return redirect("/")
+
+# @app.route("/", methods=["GET"])
+# def render_home_page():
+#     render_template("homepage.html")
+
+@app.route("/")
+def home_page():
+    db = get_db()
+    today = date.today()
+    # db.execute("INSERT INTO events (id, title, message, date, location, theme) VALUES (:id, :title, :message, :date, :location, :theme);", {"id":id, "title":title, "message":message, "date":date, "location":location, "theme":theme})
+    rows = db.execute("SELECT title, message, date, location FROM events WHERE date < ;")
+    db.commit()
+    return render_template("home_page.html", rows=rows)
