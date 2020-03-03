dcc8c33a7035305387c1a7a81cb538016bf963e7
Laima Urnėžaitė <laima@Laimas-MacBook-Pro.local>
2020-03-03 15:42:09 +0200

    implement dummy home page

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ from datetime import date
 
 app = Flask(__name__)
 
+
 def create_database(db_file):
     # create a database connection to a SQLite database
     conn = None
     try:
         conn = sqlite3.connect(db_file)
         db = conn.cursor()
-        db.execute("CREATE TABLE IF NOT EXISTS events2 (id INTEGER NOT NULL, title TEXT NOT NULL, message TEXT, year TEXT NOT NULL, month TEXT NOT NULL, day TEXT NOT NULL, location TEXT, theme TEXT NOT NULL); ")
+        db.execute("CREATE TABLE IF NOT EXISTS events (person_id INTEGER NOT NULL, event_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, title TEXT NOT NULL, message TEXT, year TEXT NOT NULL, month TEXT NOT NULL, day TEXT NOT NULL, location TEXT, theme TEXT NOT NULL);")
+
         # print(sqlite3.version)
     except Error as e:
         print(e)
@@ -22,12 +24,20 @@ def create_database(db_file):
         if conn:
             conn.close()
 
+
 create_database("countdown.db")
+
 
 def get_db():
     db = sqlite3.connect("countdown.db")
     cur = db.cursor()
     return db
+
+
+def get_cursor():
+    db = sqlite3.connect("countdown.db")
+    cur = db.cursor()
+    return cur
 
 # to print results from database
 # cur.execute("SELECT * FROM events;")
@@ -36,35 +46,37 @@ def get_db():
 # for row in rows:
 #     print(row)
 
+
 @app.route("/form/add-event", methods=["GET"])
 def render_add_event():
     return render_template("add_event.html")
 
+
 @app.route("/add-event", methods=["POST"])
 def add_event():
-    # add new event that you want to count 
+    # add new event that you want to count
     id = request.form.get("id")
     title = request.form.get("title")
     message = request.form.get("message")
-    year = request.form.get("year")
-    month = request.form.get("month")
-    day = request.form.get("day")
+    event_date = request.form.get("date")
     location = request.form.get("location")
     theme = request.form.get("theme")
     db = get_db()
-    db.execute("INSERT INTO events2 (id, title, message, year, month, day, location, theme) VALUES (:id, :title, :message, :year, :month, :day, :location, :theme);", {"id":id, "title":title, "message":message, "year":year, "month":month, "day":day, "location":location, "theme":theme})
+    db.execute("INSERT INTO events (id, title, message, date, location, theme) VALUES (:id, :title, :message, :date, :location, :theme);", {
+               "id": id, "title": title, "message": message, "date": event_date, "location": location, "theme": theme})
     db.commit()
     return redirect("/")
 
 # @app.route("/", methods=["GET"])
 # def render_home_page():
-#     render_template("homepage.html")
+#     return render_template("home_page.html")
 
-@app.route("/")
+
+@app.route("/", methods=["GET"])
 def home_page():
     db = get_db()
+    cur = get_cursor()
     today = date.today()
-    # db.execute("INSERT INTO events (id, title, message, date, location, theme) VALUES (:id, :title, :message, :date, :location, :theme);", {"id":id, "title":title, "message":message, "date":date, "location":location, "theme":theme})
-    rows = db.execute("SELECT title, message, date, location FROM events WHERE date < ;")
-    db.commit()
+    cur.execute("SELECT * FROM events where date > :today;", {"today": today})
+    rows = cur.fetchall()
     return render_template("home_page.html", rows=rows)
